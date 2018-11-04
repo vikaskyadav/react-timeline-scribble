535206872c431f3d7c4b59758d201d8dfb0f2960
vikaskyadav <vikasharry03@gmail.com>
2018-11-04 20:48:45 +0530

    removed separate components from index

--- src/index.jsx
+++ src/index.jsx
@@ -1,44 +1,20 @@
 import React, { Fragment } from "react";
 import { render } from "react-dom";
-import {Card,CardContent,CardHeader} from "@material-ui/core"
-import s from './style'
-
-
-export const Timeline = ({ children }) =>
-  <div className={s.container}>
-    <ul className={s.timeline}>
-      {children}
-    </ul>
-  </div>
-
-export const Event = ({ title, subtitle, interval, children }) =>
-  <li className={s.event}>
-    <label className={s.icon}></label>
-    <div className={s.body}>
-      <Card>
-      <div style={{"float":"right"}}>
-      <p className={s.date}>{interval}</p>
-      </div>
-      <CardHeader title={title} className={s.cardHeader} subheader={subtitle}/>
-      <CardContent className={s.description}>
-        {children}
-      </CardContent>
-      </Card>
-    </div>
-  </li>
+import {Timeline} from "./Timeline"
+import {Event} from "./Event"
 
 class App extends React.Component{
   render(){
     const events = [
-      {"title":"Pyconf 2017", "subtitle":"Hyderabad","interval":"Oct 7,2017", "description":"Held at IIIT Hyderabad. Some Talks were really good"},
-      {"title":"Pycon 2018", "subtitle":"Hyderabad","interval":"Oct 7,2018"}
+      {"title":"Pyconf 2017", "subtitle":"Hyderabad","interval":"07-10-2019", "description":"Held at IIIT Hyderabad. Some Talks were really good"},
+      {"title":"Pycon 2018", "subtitle":"Hyderabad","interval":"O7-10-2018"}
   ]
     return(
   <Fragment>
     <h1>React Timeline </h1>
     <Timeline>
       {events? events.map((event,index)=>(
-        <Event key={index} interval={event.interval} title={event.title} subtitle={event.subtitle} children={event.description}/>
+        <Event key={index} interval={event.interval} title={event.title} subtitle={event.subtitle} description={event.description}/>
       )): null
       }
     </Timeline>
